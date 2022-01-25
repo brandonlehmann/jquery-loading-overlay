38b47f0174b66b5deb861dddf5990f3f3bff44bd
Brandon Lehmann <brandonlehmann@gmail.com>
2022-01-25 09:44:03 -0500

    Fix the reference line in the jquery types extension

--- typings/loadingoverlay.d.ts
+++ typings/loadingoverlay.d.ts
@@ -1,10 +1,61 @@
-/// <reference path="../jquery/jquery.d.ts" />
+/// <reference types="jquery" />
 
-interface JQuery {
-    LoadingOverlay(action?: string, option?: boolean): any;
+interface ILoadingOverlayOptions {
+    background?: string;
+    backgroundClass?: string;
+    image?: string;
+    imageAnimation?: string;
+    imageAutoResize?: boolean;
+    imageResizeFactor?: number;
+    imageColor?: string;
+    imageClass?: string;
+    imageOrder?: number;
+    fontawesome?: string;
+    fontawesomeAnimation?: string;
+    fontawesomeAutoResize?: boolean;
+    fontawesomeResizeFactor?: number;
+    fontawesomeColor?: string;
+    fontawesomeOrder?: number;
+    custom?: string;
+    customAnimation?: string;
+    customAnimationResize?: boolean;
+    customResizeFactor?: number;
+    customOrder?: number;
+    text?: string;
+    testAnimation?: string;
+    textAutoResize?:boolean;
+    textResizeFactor?: number;
+    textColor?: string;
+    textClass?: string;
+    textOrder?: number;
+    progress?: boolean;
+    progressAutoResize?: boolean;
+    progressResizeFactor?: number;
+    progressColor?: string;
+    progressClass?: string;
+    progressOrder?: number;
+    progressFixedPosition?: string;
+    progressSpeed?: number;
+    progressMin?: number;
+    progressMax?: number;
+    size?: number;
+    maxSize?: number;
+    minSize?: number;
+    direction?: string;
+    fade?: boolean;
+    resizeInterval?: number;
+    zIndex?: number;
 }
 
+interface JQuery {
+    LoadingOverlay(
+        action: 'show' | 'hide' | 'resize' | 'text' | 'progress',
+        options?: ILoadingOverlayOptions
+    ): void;
+}
 interface JQueryStatic {
-    LoadingOverlay(action?: string, option?: boolean): void;
-    LoadingOverlaySetup(setting?: any): void;
+    LoadingOverlay(
+        action: 'show' | 'hide' | 'resize' | 'text' | 'progress',
+        options?: ILoadingOverlayOptions
+    ): void;
 }
